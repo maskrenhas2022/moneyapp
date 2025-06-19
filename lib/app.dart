23062a459ee19cdba7000dae85611f8548b5de50
MaskrenhasDev <maskrenhas02@gmail.com>
2025-06-19 16:41:42 -0400

    Start  create new folder and files.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:moneyapp/features/splash/splash_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        drawer: const Drawer(),
-
-        body: const Center(child: Text('Maskrenhas Home')),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-      ),
-    );
+    return MaterialApp(home: SplashPage());
   }
 }
---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF63B5AF), Color(0xFF438883)],
+        ),
+      ),
+    );
+  }
+}
